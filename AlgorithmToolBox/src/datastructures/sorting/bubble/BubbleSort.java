ba238ac6d4260f2edcfbeaf71e24acdfc64248b7
HagosHaile <hagosb.haile@gmail.com>
2018-06-05 15:55:28 -0400

    bubble sort

---
+++ AlgorithmToolBox/src/datastructures/sorting/bubble/BubbleSort.java
@@ -0,0 +1,25 @@
+package datastructures.sorting.bubble;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		int[] intA = new int[] { 20, 35, -15, 7, 55, 1, -22 };
+		int temp = 0;
+		int len = intA.length;
+		for (int i = 0; i < len; ) {
+			for (int j = 1; j < len; j++) {
+				if (intA[j-1] > intA[j]) {
+					temp = intA[j-1];
+					intA[j-1] = intA[j];
+					intA[j] = temp;
+				}
+			}
+			len--;
+		}
+		for (int i = 0; i < intA.length; i++) {
+			System.out.println(intA[i]);
+		}
+
+	}
+
+}
