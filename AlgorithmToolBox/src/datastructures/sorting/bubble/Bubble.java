4ff0519a985cac3d7ab7e74e60c8772c3cf67775
HagosHaile <hagosb.haile@gmail.com>
2018-06-07 10:57:55 -0400

    update

--- AlgorithmToolBox/src/datastructures/sorting/bubble/Bubble.java
+++ AlgorithmToolBox/src/datastructures/sorting/bubble/Bubble.java
@@ -4,7 +4,18 @@ public class Bubble {
 
 	public static void main(String[] args) {
 		int[] intA = new int[] { 20, 35, -15, 7, 55, 1, -22 };
+		
+		
 
+	}
+	
+	public static void swap(int[] intA, int i, int j){
+		
+		if(i==j) return;
+		int temp = intA[i];
+		intA[i]= intA[j];
+		intA[j] = temp;
+		
 	}
 
 }
