eabfdc9df03cfb583c53759c0a75885384d52d14
Hagos Haile <hagosb.haile@gmail.com>
2021-08-25 10:49:10 -0500

    code refactor

---
+++ AlgorithmToolBox/src/algorithms/array/GetNumber.java
@@ -0,0 +1,16 @@
+package algorithms.array;
+
+public class GetNumber {
+
+	public static void main(String[] args) {
+		String value = "propDamDed500usd";
+		String strConst = "propDamDed";
+		String usd = "usd";
+		
+		
+		
+		
+
+	}
+
+}
