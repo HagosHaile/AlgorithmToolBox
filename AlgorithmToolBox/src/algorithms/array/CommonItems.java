66d16abfcac6889d58b695844ad3fd54eaa974cc
Hagos Haile <hagosb.haile@gmail.com>
2021-08-31 20:28:58 -0500

    brute force solution if two arrays have common int values

---
+++ AlgorithmToolBox/src/algorithms/array/CommonItems.java
@@ -0,0 +1,24 @@
+package algorithms.array;
+
+public class CommonItems {
+
+	public static void main(String[] args) {
+		boolean haveCommonItem = containsCommonItem(new int[] {1,2,3,4},new int[] {9,5,6,7});
+		System.out.println(haveCommonItem);
+
+	}
+	
+	
+	public static boolean containsCommonItem(int[] arr, int[] arr1) {
+		for(int i=0; i<arr.length; i++) {
+			for(int j=0; j<arr1.length; j++) {
+				if(arr[i]==arr1[j])
+					return true;
+			}
+		}
+		
+		return false;
+		
+	}
+
+}
