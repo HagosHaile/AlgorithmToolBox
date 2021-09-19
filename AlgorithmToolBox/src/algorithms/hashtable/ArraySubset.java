d7da8376ab093514b8422d2251762d7f8523483c
Hagos Haile <hagosb.haile@gmail.com>
2021-09-19 18:56:11 -0500

    hashtable and hashMap problem

---
+++ AlgorithmToolBox/src/algorithms/hashtable/ArraySubset.java
@@ -0,0 +1,10 @@
+package algorithms.hashtable;
+
+public class ArraySubset {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ AlgorithmToolBox/src/algorithms/hashtable/impl/HashNode.java
@@ -0,0 +1,12 @@
+package algorithms.hashtable.impl;
+
+public class HashNode<K,V> {
+	K key;
+	V value;
+	HashNode<K, V>next;
+	public HashNode()
+	{
+		this.key=key;
+		this.value=value;
+	}
+}
