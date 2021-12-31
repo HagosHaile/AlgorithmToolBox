10ecccc52e4208cb685b50e17866bfa679761689
Hagos Haile <hagosb.haile@gmail.com>
2021-12-31 17:02:26 -0600

    update with insertion sort

---
+++ AlgorithmToolBox/src/algorithms/sorting/insertion/InsertionSort.java
@@ -0,0 +1,32 @@
+package algorithms.sorting.insertion;
+
+public class InsertionSort {
+
+	public static void main(String[] args) {
+		
+		int[] arr = {5,4,7,7,2,8};
+		int[] arr2 = sorted(arr);
+		
+		for(int j =0; j<arr2.length; j++) {
+			System.out.print(arr2[j]+", ");
+		}
+		
+	}
+	
+	public static int[] sorted(int[] arr) {
+	
+		for(int i=1; i<arr.length; i++) {
+			int current = arr[i];
+			int j=i-1;
+			while(j>-1 && arr[j]>current) {
+				arr[j+1] = arr[j];
+				j=j-1;
+				
+			}
+			arr[j+1]= current;
+					
+		}
+		return arr;
+	}
+
+}
