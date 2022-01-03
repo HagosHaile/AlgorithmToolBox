127cd4abfaa54103996b50a64ee80ccdc7e22062
Hagos Haile <hagosb.haile@gmail.com>
2022-01-02 19:15:30 -0600

    selection sort refactor

--- AlgorithmToolBox/src/algorithms/sorting/selection/SelectionSort.java
+++ AlgorithmToolBox/src/algorithms/sorting/selection/SelectionSort.java
@@ -3,7 +3,8 @@ package algorithms.sorting.selection;
 public class SelectionSort {
 
 	public static void main(String[] args) {
-		int[] arr = {5,4,7,7,2,8};
+		//int[] arr = {5,4,7,7,2,8};
+		int[] arr = new int[] { 20, 35, -15, 7, 55, 1, -22 };
 		int[] arr2 = sorted(arr);
 		
 		for(int j =0; j<arr2.length; j++) {
@@ -16,12 +17,14 @@ public class SelectionSort {
 		for(int i=0; i<arr.length-1; i++) {
 			min = arr[i];
 			int j=i+1;
+			int minIndex =i;
 			for(; j<arr.length; j++) {
 				if(min>arr[j]) {
 					min = arr[j];
+					minIndex=j;
 				}
 			}
-			arr[j]= arr[i];
+			arr[minIndex]= arr[i];
 			arr[i]=min;
 		}
 		
