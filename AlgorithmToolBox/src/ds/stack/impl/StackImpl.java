d0017446fd74c6a3dae2006b0de509453cf483db
Hagos Haile <hagosb.haile@gmail.com>
2022-01-08 14:08:10 -0600

    code refactor

--- AlgorithmToolBox/src/ds/stack/impl/StackImpl.java
+++ AlgorithmToolBox/src/ds/stack/impl/StackImpl.java
@@ -1,19 +1,25 @@
 package ds.stack.impl;
 
 public class StackImpl {
-
+	/*
+	 * time complexity of stack array implementation. 
+	 * access O(n) 
+	 * search O(n)
+	 * insertion O(1) 
+	 * deletion O(1)
+	 */
 	public static void main(String[] args) {
 		Stack stack = new Stack(5);
 
-	    stack.push(1);
-	    stack.push(2);
-	    stack.push(3);
-	    stack.push(4);
+		stack.push(1);
+		stack.push(2);
+		stack.push(3);
+		stack.push(4);
 
-	    stack.pop();
-	    System.out.println("\nAfter popping out");
+		stack.pop();
+		System.out.println("\nAfter popping out");
 
-	    stack.printStack();
+		stack.printStack();
 
 	}
 
